b8ba8420f6b6dc382d1aa414ff9b18ab8fea6c1e
Elliot Braem <elliot@ejlbraem.com>
2025-11-11 15:31:39 -0600

    remove unused plugins

--- plugins/wormhole-fati/tests/types.d.ts
+++
@@ -1,8 +0,0 @@
-import pluginDevConfig from "../plugin.dev";
-import Plugin from "@/index";
-
-declare module "every-plugin" {
-  interface RegisteredPlugins {
-    [pluginDevConfig.pluginId]: typeof Plugin;
-  }
-}
